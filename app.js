ab4d0bba34c6b506807034b9a77f268640fb0557
alexander.poltoratskiy <myownsumm@gmail.com>
2018-07-02 22:24:00 +0300

    base static server implemented

---
+++ app.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let path = require('path');
+
+let app = express();
+
+
+// Serve folder with app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(500);
+
+    return res.json({
+        message: err.message
+    });
+});
+
+app.listen(7849, () => {
+    console.log('Static backend started. Port: ', 7849);
+});
